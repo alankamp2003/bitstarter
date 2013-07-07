4291fc945f84adc0a8ae4edf927a3756364627c3
Alankar Kampoowale <alankamp2003@yahoo.com>
2013-07-07 18:16:32 +0000

    First attempt to change web.js like the way it should be for hw3

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fc = fs.readFileSync('web.js');
+  var buf = new Buffer(fc, "utf-8");   
+  reponse.send(buf.toString());
+  //response.send('Hello World 2!')
 });
 
 var port = process.env.PORT || 5000;
